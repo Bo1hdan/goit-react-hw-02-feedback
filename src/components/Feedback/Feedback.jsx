0a31081cad8265a56c42252ece937d47bbf5bc95
Bo1hdan <87267253+Bo1hdan@users.noreply.github.com>
2023-08-03 14:24:05 -0600

    Feedback is done

---
+++ src/components/Feedback/Feedback.jsx
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import Statistics from 'components/Statistics/Statistics';
+import FeedbackOptions from 'components/FeedbackOptions/FeedbackOptions';
+import Section from 'components/Section/Section';
+
+class Feedback extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleFeedback = type => {
+    this.setState(prevState => ({
+      [type]: prevState[type] + 1,
+    }));
+  };
+
+  countTotalFeedback = () => {
+    const { good, bad, neutral } = this.state;
+    return good + bad + neutral;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const { good } = this.state;
+    const totalFeedback = this.countTotalFeedback();
+
+    if (totalFeedback === 0) {
+      return 0;
+    }
+
+    return (good / totalFeedback) * 100;
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    const totalFeedback = this.countTotalFeedback();
+    const positiveFeedback = this.countPositiveFeedbackPercentage();
+    const feedbackKeys = Object.keys(this.state);
+
+    return (
+      <div className="wrapper">
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={feedbackKeys}
+            onLeaveFeedback={this.handleFeedback}
+          />
+        </Section>
+        <Section title="Statistics">
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={totalFeedback}
+            positivePercentage={positiveFeedback}
+          />
+        </Section>
+      </div>
+    );
+  }
+}
+
+export default Feedback;
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Notification from 'components/Notification/Notification';
+
+const Statistics = ({ good, neutral, bad, total, positivePercentage }) => {
+  if (total === 0) {
+    return <Notification message="There is no feedback" />;
+  }
+
+  return (
+    <div>
+      <h2>Statistics</h2>
+      <ul className="feedbackList">
+        <li>Good: {good}</li>
+        <li>Neutral: {neutral}</li>
+        <li>Bad: {bad}</li>
+        <li>Total: {total}</li>
+        <li>Positive feedback: {positivePercentage.toFixed(2)}%</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Statistics;
